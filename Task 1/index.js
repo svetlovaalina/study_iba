3aaeaa3ac1fb72d47e3124ba0b778a22d7570281
svetlovaalina <svetlova.alina33@mail.ru>
2021-02-16 11:08:04 +0300

    Done task 1 about js array methods

---
+++ Task 1/index.js
@@ -0,0 +1,40 @@
+const users = [{
+    name: 'Eugene',
+    surname: 'Vasil',
+    age: 27,
+  },
+  {
+    name: 'Alina',
+    surname: 'Gospodarik',
+    age: 17,
+  },
+  {
+    name: 'Max',
+    surname: 'Sotnikov',
+    age: 33,
+  },
+  {
+    name: 'Nick',
+    surname: 'Tester',
+    age: 5,
+  },
+  {
+    name: 'Sveta',
+    surname: 'Developer',
+    age: 55,
+  },
+];
+
+
+
+
+let sortByAge = function (a, b) {
+  return a.age > b.age ? 1 : -1;
+}
+
+let resMapUsers = users.map(function (item) {
+  item.fullName = `${item.name} ${item.surname}`;
+  return item;
+}).sort(sortByAge);
+
+console.log(resMapUsers);
