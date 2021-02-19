3c25fbdcb4e2e085dca97c4e487df8bfd0862d08
svetlovaalina <svetlova.alina33@mail.ru>
2021-02-19 20:52:10 +0300

    Done task about fetch

---
+++ task 4 fetch/script.js
@@ -0,0 +1,40 @@
+// Избавиться от документ рэди 
+// Заставить отработать фетч 
+// только этого можно разрешить работать jquery
+
+fetch('https://dog.ceo/api/breed/hound/images')
+    .then(response => response.json())
+    .then(responseResult => {
+
+        let arr = responseResult.message;
+        arr.length = 100;
+        console.log(responseResult);
+        let containerJS = document.getElementsByClassName('message_list')[0]
+        arr.forEach(image => {
+            let img = document.createElement('img');
+            let li = document.createElement('li');
+            img.style = 'margin:10px';
+            img.src = image;
+            li.appendChild(img);
+            containerJS.append(li);
+        });
+        funAfterFetch()
+    })
+
+let counter = 0;
+const counterFun = () => 5 * (counter + 2)
+
+function funAfterFetch() {
+
+    //hide message li after the 5th
+    $(".message_list li:gt(4)").hide();
+
+
+    //show all messages
+    $(".show_next5_message").click(function () {
+
+        $(`.message_list li:lt(${counterFun()})`).slideDown()
+        counter++;
+        return false;
+    });
+}
