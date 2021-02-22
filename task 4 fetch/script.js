81a3bcadfd77f7debd49f9a503330ed541d0b24b
svetlovaalina <svetlova.alina33@mail.ru>
2021-02-22 12:30:53 +0300

    Modified task pagination

--- task 4 fetch/script.js
+++ task 4 fetch/script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-// Избавиться от документ рэди 
-// Заставить отработать фетч 
-// только этого можно разрешить работать jquery
->>>>>>> 3c25fbdcb4e2e085dca97c4e487df8bfd0862d08
 
 fetch('https://dog.ceo/api/breed/hound/images')
     .then(response => response.json())
@@ -18,10 +12,7 @@ fetch('https://dog.ceo/api/breed/hound/images')
             let li = document.createElement('li');
             img.style = 'margin:10px';
             img.src = image;
-<<<<<<< HEAD
 
-=======
->>>>>>> 3c25fbdcb4e2e085dca97c4e487df8bfd0862d08
             li.appendChild(img);
             containerJS.append(li);
         });
@@ -36,15 +27,9 @@ function funAfterFetch() {
     //hide message li after the 5th
     $(".message_list li:gt(4)").hide();
 
-
-<<<<<<< HEAD
-    //show next 5 messages
-    $(".show_next5_message").click(function () {
-=======
     //show all messages
     $(".show_next5_message").click(function () {
 
->>>>>>> 3c25fbdcb4e2e085dca97c4e487df8bfd0862d08
         $(`.message_list li:lt(${counterFun()})`).slideDown()
         counter++;
         return false;
