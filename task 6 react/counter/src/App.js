9a1596af3f494c0f1762741d3c114ce5d55827a7
svetlovaalina <svetlova.alina33@mail.ru>
2021-02-26 00:27:55 +0300

    Partly done tast with hooks

--- task 6 react/counter/src/App.js
+++ task 6 react/counter/src/App.js
@@ -1,26 +1,25 @@
 import React, {Component} from 'react';
 import classes from './App.module.css';
-import { Counter } from './components/Counter';
+import {Counter} from './components/Counter';
 
 
 class App extends Component {
-
-  state = {
-    isVisible: true
-  }
-
-
 
   render() {
     return (
       <div className={classes.App}>
         <h1 className={classes.Header}>При каждом пятом нажатии на кнопку она меняет цвет:</h1>
-        {this.state.isVisible && <Counter count={2} />}
-        <button onClick={() => this.setState({isVisible: false})}>Hide</button>
-
-      </div>
+        < Counter />
+     
+      </div>  
     );
   }
 }
 
 export default App;
+
+
+// .............После тега h1...................................................................
+  {/* {this.state.isVisible && <Counter count={2} />}
+        <button onClick={() => this.setState({isVisible: false})}>Hide</button> 
+        */}
--- task 6 react/counter/src/components/Counter/Counter.js
+++ task 6 react/counter/src/components/Counter/Counter.js
@@ -1,64 +1,156 @@
-import React from 'react';
+import React , {useEffect, useState} from 'react';
 import classes from './Counter.module.css';
 
-export class Counter extends React.Component {
-  state = {
-    backgroundColor: '#B3CCFF',
-  };
 
-  constructor(props){
-    super(props);
-    console.log('constructor');
+function Counter() {
+const [count, setCounter] = useState(0);
+  const [color, setColor] = useState('rgb(97, 255, 255)');
+ let divStyle = { backgroundColor: color};
+
+function handleClick() {
+  setCounter( count + 1)
+  if ((count + 1) % 5 === 0 && count > 1) {
+    const randomColor = "#"+((1<<24) * Math.random()|0).toString(16);
+    setColor(randomColor) ;
+  }};
+  
+  React.useEffect( () =>
+  { console.log('Messege')
+    
   }
+  )
+  return (
+  <div>
+    <button onClick={handleClick} className={classes.buttonStyle} style={divStyle}> Hi! {count}</button>
+    
+  </div>
+  
+  );
+ 
+};
 
-  componentDidMount() {
-    console.log('componentDidMount');
-  }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    console.log('getDerivedStateFromProps');
-    if (!state.count) {
-      return {
-        ...state,
-        count: props.count,
-      }
-    }
-
-    return state;
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-  }
+ export {Counter}
 
 
-  handleClick = () => {
+ 
 
-    this.setState(prevState => ({
-      count: prevState.count + 1
-    }));
+// };
 
-    if ((this.state.count + 1) % 5 === 0 && this.state.count > 1) {
-      const randomColor = "#"+((1<<24) * Math.random()|0).toString(16);
+ 
+    // <button
+    //     className={classes.button}
+    //     style={{backgroundColor: props.backgroundColor}}
+    //     onClick={handleClick}>{state.count} 
+    //   </button>
 
-      this.setState({backgroundColor: randomColor});
-    }
 
-  };
 
-  render() {
-    console.log('render');
-    return (
-      <button
-        className={classes.button}
-        style={{backgroundColor: this.state.backgroundColor}}
-        onClick={this.handleClick}>
-        {this.state.count}
-      </button>
-    )
-  }
-}
+
+
+
+// export class Counte  r extends React.Component {
+//   state = {
+//     count:0,
+//     backgroundColor: '#B3CCFF',
+//   };
+
+  
+
+//   handleClick = () => {
+
+//     this.setState(prevState => ({
+//       count: prevState.count + 1
+//     }));
+
+//     if ((this.state.count + 1) % 5 === 0 && this.state.count > 1) {
+//       const randomColor = "#"+((1<<24) * Math.random()|0).toString(16);
+
+//       this.setState({backgroundColor: randomColor});
+//     }
+
+//   };
+
+//   render() {
+//     console.log('render');
+//     return (
+//       <button
+//         className={classes.button}
+//         style={{backgroundColor: this.state.backgroundColor}}
+//         onClick={this.handleClick}>
+//          {this.state.count} 
+//       </button>
+//     )
+//   }
+// }
+
+
+
+
+
+
+// ..............................................................................................................
+// import React from 'react';
+// import classes from './Counter.module.css';
+
+// export class Counter extends React.Component {
+//   state = {
+//     count:0,
+//     backgroundColor: '#B3CCFF',
+//   };
+
+//   constructor(props){
+//     super(props);
+//     console.log('constructor');
+//   }
+
+//   componentDidMount() {
+//     console.log('componentDidMount');
+//   }
+
+//   componentDidUpdate() {
+//     console.log('componentDidUpdate');
+//   }
+
+//   // static getDerivedStateFromProps(props, state) {
+//   //   console.log('getDerivedStateFromProps');
+//   //   if (!state.count) {
+//   //     return {
+//   //       ...state,
+//   //       count: props.count,
+//   //     }
+//   //   }
+
+//   //   return state;
+//   // }
+
+//   componentWillUnmount() {
+//     console.log('componentWillUnmount');
+//   }
+
+
+//   handleClick = () => {
+
+//     this.setState(prevState => ({
+//       count: prevState.count + 1
+//     }));
+
+//     if ((this.state.count + 1) % 5 === 0 && this.state.count > 1) {
+//       const randomColor = "#"+((1<<24) * Math.random()|0).toString(16);
+
+//       this.setState({backgroundColor: randomColor});
+//     }
+
+//   };
+
+//   render() {
+//     console.log('render');
+//     return (
+//       <button
+//         className={classes.button}
+//         style={{backgroundColor: this.state.backgroundColor}}
+//         onClick={this.handleClick}>
+//          {this.state.count} 
+//       </button>
+//     )
+//   }
+// }
