9a11d3e8d635678dab8a1842bb0ee0c2358bf702
Eugene <yauhen.vasileuski1@ibm.com>
2021-03-01 15:03:04 +0300

    Some fixes

--- react-project/my-project/src/components/layouts/PhoneCard/PhoneCard.js
+++ react-project/my-project/src/components/layouts/PhoneCard/PhoneCard.js
@@ -5,7 +5,9 @@ export const PhoneCard = ({ name, snippet, imageUrl, id, className }) => {
   return (
     <div className={className}>
       <div className={classes.container}>
-        <img alt="Phone image" src={'http://angular.github.io/angular-phonecat/step-14/app/' + imageUrl}/>
+        <div className={classes.imageContainer}>
+          <img className={classes.image} alt="Phone image" src={'http://angular.github.io/angular-phonecat/step-14/app/' + imageUrl}/>
+        </div>
         <div className={classes.mobileDescription}>
           <a href="#" className={classes.mobileName}>{name} </a>
           <p className={classes.mobileSnippet}>{snippet}</p>
