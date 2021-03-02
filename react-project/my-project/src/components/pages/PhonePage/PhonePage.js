81566879ec046e58342e30d599c327b03aba3e02
svetlovaalina <svetlova.alina33@mail.ru>
2021-03-02 11:50:24 +0300

    Tried to add Carousel

--- react-project/my-project/src/components/pages/PhonePage/PhonePage.js
+++ react-project/my-project/src/components/pages/PhonePage/PhonePage.js
@@ -2,18 +2,34 @@ import React from 'react';
 import classes from './PhonePage.module.css'
 import {useParams} from "react-router-dom";
 import useFetch from "react-fetch-hook";
+// import { Carousel } from 'react-responsive-carousel';
 
 export const PhonePage = () => {
     const {id} = useParams()
     const {isLoading, error, data: phoneData} = useFetch(`http://angular.github.io/angular-phonecat/step-14/app/phones/${id}.json`)
-        if (isLoading) return "Loading!";
-        if (error) return "Error loading data!";
- 
+       if (isLoading) return "Loading!";
+       if (error) return "Error loading data!";
+       
     return (
-      <div className={classes.container}>
-          <h1> id:  {id} </h1>
-          
-      </div>)
+    <div className={classes.phoneImages}>
+        <h1> id:  {id} </h1>
+        <div className={classes.imageContainer}>
+      
+      {/* <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}> 
+     
+        {phoneData.images.map((item, i) =>
+            {
+            <div>
+                <img src={`http://angular.github.io/angular-phonecat/step-14/app/img/phones/${item.id}.${item.Carouseli}.jpg`} />
+                <p className="legend">Legend 1</p>
+            </div>
+            }   
+        )}            
+      </Carousel> */}
+        </div>
+    </div>
+    
+    )
   };
   
   
