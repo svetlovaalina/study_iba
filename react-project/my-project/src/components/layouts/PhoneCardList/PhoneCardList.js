3c6a86b776da6ce57e4679c92092299b8a1c768b
svetlovaalina <svetlova.alina33@mail.ru>
2021-03-03 12:14:18 +0300

    Added style for PhoneCard

--- react-project/my-project/src/components/layouts/PhoneCardList/PhoneCardList.js
+++ react-project/my-project/src/components/layouts/PhoneCardList/PhoneCardList.js
@@ -6,28 +6,25 @@ import Spinner from 'react-bootstrap/Spinner';
 import Alert from 'react-bootstrap/Alert';
 import {Context} from "../../../App";
 
-
-
 export const PhoneCardList = () => {
     const {phoneList, setPhoneList} = useContext(Context);
-    
+
     let {isLoading, error, data: list} = useFetch('http://angular.github.io/angular-phonecat/step-14/app/phones/phones.json')
-    
+
     if (isLoading) 
         return (<Spinner animation="border" className='spinner'/>);
     if (error) {
         return (
-            <Alert variant='danger'>"Error : {error.message}   {error.status}"
+            <Alert variant='danger'>"Error : {error.message}
+                {error.status}"
             </Alert>
         );
     }
-   if (isLoading === false) {
+    if (isLoading === false) {
         setPhoneList(list)
-   }
-   
+    }
 
     return (
-
         <div className={classes.container}>
 
             {phoneList.map(item => <PhoneCard
