06201ecbc3a1a5178bd4bbdd995f53f2be301897
svetlovaalina <svetlova.alina33@mail.ru>
2021-03-09 12:06:05 +0300

    Added Align items

--- react-project/my-project/src/components/layouts/PhoneCard/PhoneCard.js
+++ react-project/my-project/src/components/layouts/PhoneCard/PhoneCard.js
@@ -4,20 +4,20 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 export const PhoneCard = ({name, snippet, imageUrl, id, className}) => {
     return (
-        <div className={className}>
-            <div className={classes.container}>
-                <div className={classes.imageContainer}>
-                    <img
-                        className={classes.image}
-                        alt="Phone image"
-                        src={'http://angular.github.io/angular-phonecat/step-14/app/' + imageUrl}/>
-                </div>
-                <div className={classes.mobileDescription}>
-                    <Link to={"/phonePage/" + id} className={classes.mobileName}>{name}
-                    </Link>
-                    <p className={classes.mobileSnippet}>{snippet}</p>
-                </div>
+
+        <div className={classes.container}>
+            <div className={classes.imageContainer}>
+                <img
+                    className={classes.image}
+                    alt="Phone image"
+                    src={'http://angular.github.io/angular-phonecat/step-14/app/' + imageUrl}/>
+            </div>
+            <div className={classes.mobileDescription}>
+                <Link to={"/phonePage/" + id} className={classes.mobileName}>{name}
+                </Link>
+                <p className={classes.mobileSnippet}>{snippet}</p>
             </div>
         </div>
+
     )
 };
