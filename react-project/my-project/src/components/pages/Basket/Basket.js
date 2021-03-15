c1e750e9f1c043fc94295de49fc931ab487dfd17
svetlovaalina <svetlova.alina33@mail.ru>
2021-03-15 23:00:08 +0300

    Added modern Delete confirmation

--- react-project/my-project/src/components/pages/Basket/Basket.js
+++ react-project/my-project/src/components/pages/Basket/Basket.js
@@ -5,46 +5,65 @@ import {PhoneCard} from '../../layouts/PhoneCard';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert';
+import Modal from 'react-bootstrap/Modal'
 
 export const Basket = () => {
+    const [show,
+        setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const [localStorageBasket,
         setLocalStorageBasket] = useState(JSON.parse(localStorage.getItem('phoneListBasket')))
 
     const deleteButton = (event) => {
-        if (window.confirm('Are you sure you wish to delete this item?')){
         const idPhone = event
             .currentTarget
             .getAttribute('idPhone');
-        setLocalStorageBasket(localStorageBasket.filter((el) => el.id !== idPhone))}
+        setLocalStorageBasket(localStorageBasket.filter((el) => el.id !== idPhone))
+
     }
-    
-    
+
     useEffect(() => {
         localStorage.setItem("phoneListBasket", JSON.stringify(localStorageBasket))
     }, [localStorageBasket])
 
-
     return (
         <div className={classes.container}>
-        
-            {localStorageBasket && localStorageBasket.length ? localStorageBasket.map(item => 
-            <div className={classes.phoneCardBasket}>
-                <PhoneCard
-                    className={classes.localStorageBasket}
-                    key={item.id}
-                    id={item.id}
-                    name={item.name}
-                    imageUrl={item.images[0]}
-                    snippet={item.description}/>
-                <Button variant="danger" idPhone={item.id} onClick={deleteButton}>
-                    <DeleteIcon/>
-                </Button>
 
-            </div>) : <Alert variant='danger' className={classes.alert}>
-            Oops! Your cart is empty!ё
-            </Alert>
-            } 
+            {localStorageBasket && localStorageBasket.length
+                ? localStorageBasket.map(item => <div className={classes.phoneCardBasket}>
+                    <PhoneCard
+                        className={classes.localStorageBasket}
+                        key={item.id}
+                        id={item.id}
+                        name={item.name}
+                        imageUrl={item.images[0]}
+                        snippet={item.description}/>
+                    <Button variant="danger" idPhone={item.id} onClick={handleShow}>
+                        <DeleteIcon/>
+                    </Button>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>
+                                Remove phone</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Are you sure you want to delete this phone from the basket?</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Cancel
+                            </Button>
+                            <Button variant="primary" idPhone={item.id} onClick={deleteButton}>
+                                Delete
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>)
+                : <Alert variant='danger' className={classes.alert}>
+                    Oops! Your cart is empty!
+                </Alert>
+}
         </div>
-    ) 
+    )
 };
