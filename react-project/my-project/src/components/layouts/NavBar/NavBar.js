0e8f4a533add4d595aadcfe715acff8fc88d3765
svetlovaalina <svetlova.alina33@mail.ru>
2021-03-17 01:39:34 +0300

    Tried to added google sign-in

--- react-project/my-project/src/components/layouts/NavBar/NavBar.js
+++ react-project/my-project/src/components/layouts/NavBar/NavBar.js
@@ -8,6 +8,15 @@ import {Basket} from "../../pages/Basket";
 
 export const NavBar = () => {
 
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+
+   
     return (
         <div className={classes.container}>
             <Navbar bg="primary" variant="dark" class="d-flex justify-content-between">
@@ -21,7 +30,11 @@ export const NavBar = () => {
                             Basket</Link>
                     </Navbar.Brand>
                 </div>
+                <div class="g-signin2" data-onsuccess={onSignIn}></div>
             </Navbar>
+          
+  
+
         </div>
     )
 }
